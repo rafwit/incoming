0aa1d14517985173a364a748a5fb60cbd7e4fd4f
malkeeszon <rwitczak89@gmail.com>
2020-11-13 20:02:32 +0100

    chore(main): refactors folder structure
    refactors folder structure to get ready for publishing to npm v 1.0.0.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
 };
---
+++ main/ts/index.ts
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+/* eslint-disable no-unused-vars */
+import Contains from './decorators/Contains';
+import IsBoolean from './decorators/IsBoolean';
+import IsEmail from './decorators/IsEmail';
+import IsLength from './decorators/IsLength';
+import NotEmpty from './decorators/NotEmpty';
+import IsDate from './decorators/IsDate';
+
+export {
+  Contains, IsBoolean, IsEmail, IsLength, NotEmpty, IsDate,
+};
