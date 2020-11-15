c9edd2732e94c399a5a2bd3a21a0e3d5225d1e31
malkeeszon <rwitczak89@gmail.com>
2020-11-15 18:00:26 +0100

    fix(onlyletters): fixes decorator to throw error if used on type !== string

--- main/ts/decorators/OnlyLetters.ts
+++ main/ts/decorators/OnlyLetters.ts
@@ -4,12 +4,14 @@ import propertyMapper from '../helpers/propertyMapper';
 export default function OnlyLetters() {
   return function (target: any, key: string): void {
     propertyMapper(target, key, (next: string) => {
+      if (typeof next !== 'string') {
+        throw new Error(`\x1b[91mValidation failed on property: "${key}"\x1b[39m\x1b[93m - expected string, received value of type: ${typeof next}\x1b[39m`)
+      }
       const regex = /^[a-zA-Z]+$/;
       if (!regex.test(next)) {
-        throw new Error(`\x1b[91mValidation Error:\x1b[39m on ${target}[${key}] - recevied value: ${next} - expected only letters`);
-      } else {
-        return next;
+        throw new Error(`\x1b[91mValidation failed on property: "${key}"\x1b[39m\x1b[93m recevied value: ${next} - expected only letters\x1b[39m`);
       }
+      else return next;
     });
   };
 }
