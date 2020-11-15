51bb6ebea0c5e8e35923074f82355b3329ebf7bd
malkeeszon <rwitczak89@gmail.com>
2020-11-15 13:51:41 +0100

    fix(isnumber.ts): fixes type declaration
    fixes type declaration of checked value from string to number for extra check in TS

--- main/ts/decorators/IsNumber.ts
+++ main/ts/decorators/IsNumber.ts
@@ -3,7 +3,7 @@ import propertyMapper from '../helpers/propertyMapper';
 export default function IsNumber() {
   // eslint-disable-next-line func-names
   return function (target: any, key: string): void {
-    propertyMapper(target, key, (next: string) => {
+    propertyMapper(target, key, (next: number) => {
       if (typeof next !== 'number') {
         throw new Error(`\x1b[91mValidation Error:\x1b[39m on ${target}[${key}] - should be of type Number`);
       } else {
