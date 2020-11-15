06797ac5e467b2ba846dc334baa7b8bf45df1715
malkeeszon <rwitczak89@gmail.com>
2020-11-15 17:57:53 +0100

    fix(maxlength): fixes decorator to throw err if used on type !== string or array

--- main/ts/decorators/MaxLength.ts
+++ main/ts/decorators/MaxLength.ts
@@ -2,12 +2,14 @@ import propertyMapper from '../helpers/propertyMapper';
 
 export default function MaxLength(max: number) {
   return function (target: any, key: string): void {
-    propertyMapper(target, key, (next: string) => {
-      if (next.length > max) {
-        throw new Error(`\x1b[91mValidation Error:\x1b[39m on ${target}[${key}] - value has length: ${next.length} - should have max ${max} length`);
-      } else {
-        return next;
+    propertyMapper(target, key, (next: string | string[] | number[] | object[]) => {
+      if (!(typeof next === 'string' || Array.isArray(next))) {
+        throw new Error(`\x1b[91mValidation failed on property: "${key}"\x1b[39m\x1b[93m - value of type: ${typeof next}, does not have length\x1b[39m`)
       }
+      if (next.length > max) {
+        throw new Error(`\x1b[91mValidation failed on property: "${key}"\x1b[39m \x1b[93m - value has length: ${next.length}, should have length max: ${max}\x1b[39m`);
+      }
+      else return next;
     });
   };
 }
