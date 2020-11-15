c63582c1b2a7137425169aaa22f8a16512bb8040
malkeeszon <rwitczak89@gmail.com>
2020-11-15 17:51:59 +0100

    refactor(isboolean): refactors error message to be more precise

--- main/ts/decorators/IsBoolean.ts
+++ main/ts/decorators/IsBoolean.ts
@@ -1,11 +1,10 @@
 import propertyMapper from '../helpers/propertyMapper';
 
 export default function IsBoolean() {
-  // eslint-disable-next-line func-names
   return function (target: any, key: string): void {
     propertyMapper(target, key, (next: boolean) => {
       if (next !== true && next !== false) {
-        throw new Error(`\x1b[91mValidation Error:\x1b[39m on ${target}[${key}] - should be Boolean`);
+        throw new Error(`\x1b[91mValidation failed on property: "${key}"\x1b[39m\x1b[93m - expected boolean, received value of type: ${typeof next}\x1b[39m`);
       } else {
         return next;
       }
