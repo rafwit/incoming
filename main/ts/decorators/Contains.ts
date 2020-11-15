69b1a72e1c7175172ea6dcf410d591dee969f4bc
malkeeszon <rwitczak89@gmail.com>
2020-11-15 17:48:02 +0100

    fix(contains): fixes decorator to throw err if used on type !== string

--- main/ts/decorators/Contains.ts
+++ main/ts/decorators/Contains.ts
@@ -1,11 +1,14 @@
 import propertyMapper from '../helpers/propertyMapper';
 
 export default function Contains(word: string) {
-    return function (target: any, key: string): void {
-        propertyMapper(target, key, (next: string) => {
-            if (!next.includes(word)) {
-                throw new Error(`\x1b[91mValidation Error:\x1b[39m on ${target}[${key}] - should contain "${word}"`);
-            } else return next;
-        });
-    };
+  return function (target: any, key: string): void {
+    propertyMapper(target, key, (next: string) => {
+      if (typeof next !== 'string') {
+        throw new Error(`\x1b[91mValidation failed on property: "${key}"\x1b[39m\x1b[93m - expected string, received value of type: ${typeof next}\x1b[39m`)
+      }
+      if (!next.includes(word)) {
+        throw new Error(`\x1b[91mValidation failed on property: "${key}"\x1b[39m\x1b[93m - value should contain string: ${word}\x1b[39m`);
+      } else return next;
+    });
+  };
 }
