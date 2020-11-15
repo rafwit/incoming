dfd03787b26bde4075d52d0d50777297ba4a27b7
malkeeszon <rwitczak89@gmail.com>
2020-11-15 18:01:48 +0100

    fix(isuppercase): fixes decorator to throw error if used on type !== string

--- main/ts/decorators/isUppercase.ts
+++ main/ts/decorators/isUppercase.ts
@@ -1,11 +1,15 @@
 import propertyMapper from '../helpers/propertyMapper';
 
 export default function IsUpperCase() {
-    return function (target: any, key: string): void {
-        propertyMapper(target, key, (next: string) => {
-            if (next !== next.toUpperCase()) {
-                throw new Error(`\x1b[91mValidation Error:\x1b[39m on ${target}[${key}] - expected all upper case, received ${next}`);
-            } else return next;
-        });
-    };
+	return function (target: any, key: string): void {
+    propertyMapper(target, key, (next: string) => {
+        if (typeof next !== 'string') {
+            throw new Error(`\x1b[91mValidation failed on property: "${key}"\x1b[39m\x1b[93m - expected string, received value of type: ${typeof next}\x1b[39m`)
+            }
+        if (next.toUpperCase() !== next) {
+            throw new Error(`\x1b[91mValidation failed on property: "${key}"\x1b[39m\x1b[93m - expected all upper case, received ${typeof next}: ${next}\x1b[39m`);
+        }
+        else return next;
+    });
+};
 }
