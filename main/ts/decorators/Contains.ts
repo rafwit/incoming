6e10f2361cb28436a1f239bfa221ae4bd883aa15
malkeeszon <rwitczak89@gmail.com>
2020-11-15 13:19:39 +0100

    style(contains.ts): deletes eslint disable options after adjusting eslint

--- main/ts/decorators/Contains.ts
+++ main/ts/decorators/Contains.ts
@@ -1,12 +1,11 @@
 import propertyMapper from '../helpers/propertyMapper';
 
 export default function Contains(word: string) {
-  // eslint-disable-next-line func-names
-  return function (target: any, key: string): void {
-    propertyMapper(target, key, (next: string) => {
-      if (!next.includes(word)) {
-        throw new Error(`\x1b[91mValidation Error:\x1b[39m on ${target}[${key}] - should contain "${word}"`);
-      } else return next;
-    });
-  };
+    return function (target: any, key: string): void {
+        propertyMapper(target, key, (next: string) => {
+            if (!next.includes(word)) {
+                throw new Error(`\x1b[91mValidation Error:\x1b[39m on ${target}[${key}] - should contain "${word}"`);
+            } else return next;
+        });
+    };
 }
