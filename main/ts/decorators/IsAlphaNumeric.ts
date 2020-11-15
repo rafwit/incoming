4c8c2cf1ded21defb0fce9dcdc5ebdcdc092e405
malkeeszon <rwitczak89@gmail.com>
2020-11-15 17:43:32 +0100

    chore(isalphanumeric): changes names from AlphaNumeric to IsAlphaNumeric to match the method name

--- main/ts/decorators/IsAlphaNumeric.ts
+++ main/ts/decorators/IsAlphaNumeric.ts
@@ -1,11 +1,11 @@
 import propertyMapper from '../helpers/propertyMapper';
 
-export default function IsAlphaNumeric (value: string) {
+export default function IsAlphaNumeric () {
   return function (target: any, key: string): void {
     propertyMapper(target, key, (next: string) => {
       const regex = /^([0-9]|[a-z])+([0-9a-z]+)$/i;
       if (!regex.test(next)) {
-        throw new Error(`\x1b[91mValidation Error:\x1b[39m on ${target}[${key}] - value schould be alphanumeric only`);
+        throw new Error(`\x1b[91mValidation failed on property: "${key}"\x1b[39m \x1b[93m - value schould be alphanumeric only\x1b[39m`);
       } else {
         return next;
       }
