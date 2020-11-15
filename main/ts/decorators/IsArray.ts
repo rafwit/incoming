d0a992efc9fe83c4ec1ea9975044c2f844bf072f
malkeeszon <rwitczak89@gmail.com>
2020-11-15 17:51:19 +0100

    refactor(isarray): refactors error message to be more precise

--- main/ts/decorators/IsArray.ts
+++ main/ts/decorators/IsArray.ts
@@ -4,7 +4,7 @@ export default function IsArray() {
     return function (target: any, key: string): void {
         propertyMapper(target, key, (next: string) => {
             if (!Array.isArray(next)) {
-                throw new Error(`\x1b[91mValidation Error:\x1b[39m on ${target}[${key}] - expected Array, received ${next}`);
+                throw new Error(`\x1b[91mValidation failed on property: "${key}"\x1b[39m \x1b[93m - expected array, received ${typeof next}: ${next}\x1b[39m`);
             } else return next;
         });
     };
