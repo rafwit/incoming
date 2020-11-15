9e42f5de461eeb9e41d564966ad7f948e0c3dc51
malkeeszon <rwitczak89@gmail.com>
2020-11-15 18:03:51 +0100

    fix(index): updates import/export statements

--- main/ts/index.ts
+++ main/ts/index.ts
@@ -1,10 +1,21 @@
 import Contains from './decorators/Contains';
+import Equals from './decorators/Equals';
+import IsAlphaNumeric from './decorators/IsAlphaNumeric';
+import IsArray from './decorators/IsArray';
 import IsBoolean from './decorators/IsBoolean';
+import IsDateObj from './decorators/IsDateObj';
+import IsDecimalNum from './decorators/IsDecimalNum';
 import IsEmail from './decorators/IsEmail';
 import IsLength from './decorators/IsLength';
+import IsLowerCase from './decorators/IsLowerCase';
+import IsNumber from './decorators/IsNumber'
+import IsUpperCase from './decorators/IsUpperCase';
+import MaxLength from './decorators/MaxLength';
+import MinLength from './decorators/MinLength';
 import NotEmpty from './decorators/NotEmpty';
-import IsDate from './decorators/IsDate';
+import OnlyLetters from './decorators/OnlyLetters';
 
 export {
-  Contains, IsBoolean, IsEmail, IsLength, NotEmpty, IsDate,
+Contains, Equals, IsAlphaNumeric, IsArray, IsBoolean, IsDateObj, IsDecimalNum,
+IsEmail, IsLength, IsLowerCase, IsNumber, IsUpperCase, MaxLength, MinLength, NotEmpty, OnlyLetters
 };
