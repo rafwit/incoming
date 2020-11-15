0528349a6562ad19a28a703d0326e63ced9faa13
malkeeszon <rwitczak89@gmail.com>
2020-11-15 17:56:21 +0100

    refactor(isnumber): refactors error message to be more precise

--- main/ts/decorators/IsNumber.ts
+++ main/ts/decorators/IsNumber.ts
@@ -1,14 +1,13 @@
 import propertyMapper from '../helpers/propertyMapper';
 
 export default function IsNumber() {
-  // eslint-disable-next-line func-names
   return function (target: any, key: string): void {
     propertyMapper(target, key, (next: number) => {
       if (typeof next !== 'number') {
-        throw new Error(`\x1b[91mValidation Error:\x1b[39m on ${target}[${key}] - should be of type Number`);
-      } else {
-        return next;
+        throw new Error(`\x1b[91mValidation failed on property: "${key}"\x1b[39m\x1b[93m - expected number, received value of type: ${typeof next}\x1b[39m`);
       }
+      else return next;
+
     });
   };
 }
