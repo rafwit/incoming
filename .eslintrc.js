8660ace91fb1773ca343b3420f429193e832979b
malkeeszon <rwitczak89@gmail.com>
2020-11-15 13:09:27 +0100

    ci(eslint): adjusts eslint rules for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
-module.exports = {
+export default {
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ['airbnb-base'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
