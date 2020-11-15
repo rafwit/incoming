edd86d161815c10248cb2a705b656d79191faef1
malkeeszon <rwitczak89@gmail.com>
2020-11-15 17:52:53 +0100

    style(isemail): deletes eslint disable option after updating eslint options

--- main/ts/decorators/IsEmail.ts
+++ main/ts/decorators/IsEmail.ts
@@ -1,15 +1,15 @@
 import propertyMapper from '../helpers/propertyMapper';
 
 export default function IsEmail() {
-  // eslint-disable-next-line func-names
   return function (target: any, key: string): void {
     propertyMapper(target, key, (next: string) => {
+      if (typeof next !== 'string') {
+        throw new Error(`\x1b[91mValidation failed on property: "${key}"\x1b[39m\x1b[93m - expected string, received value of type: ${typeof next}\x1b[39m`)
+        }
       const regex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
       if (!regex.test(next)) {
         throw new Error(`\x1b[91mValidation Error:\x1b[39m on ${target}[${key}] - recevied: "${next}" - expected format: "example@email.com"`);
-      } else {
-        return next;
-      }
+      } else return next;
     });
   };
 }
