fc53758de2d58476d793098ce27033d9665ec858
malkeeszon <rwitczak89@gmail.com>
2020-11-15 17:58:31 +0100

    fix(minlength): fix decorator to throw err if used on type !== string or array

--- main/ts/decorators/MinLength.ts
+++ main/ts/decorators/MinLength.ts
@@ -2,12 +2,14 @@ import propertyMapper from '../helpers/propertyMapper';
 
 export default function MinLength(min: number) {
   return function (target: any, key: string): void {
-    propertyMapper(target, key, (next: string) => {
-      if (next.length < min) {
-        throw new Error(`\x1b[91mValidation Error:\x1b[39m on ${target}[${key}] - value has length: ${next.length} - should have min length ${min}`);
-      } else {
-        return next;
+    propertyMapper(target, key, (next: string | string[] | number[] | object[]) => {
+      if (!(typeof next === 'string' || Array.isArray(next))) {
+        throw new Error(`\x1b[91mValidation failed on property: "${key}"\x1b[39m\x1b[93m - value of type: ${typeof next}, does not have length\x1b[39m`)
       }
+      if (next.length < min) {
+        throw new Error(`\x1b[91mValidation failed on property: "${key}"\x1b[39m\x1b[93m - value has length: ${next.length}, should have length min: ${min}\x1b[39m`);
+      }
+      else return next;
     });
   };
 }
