3bc0c650b77b1f12a87a24da7289be2ca033d20f
malkeeszon <rwitczak89@gmail.com>
2020-11-15 14:04:14 +0100

    refactor(islength.ts): changes the error message to be more precise

--- main/ts/decorators/IsLength.ts
+++ main/ts/decorators/IsLength.ts
@@ -5,7 +5,7 @@ export default function IsLength(min: number, max: number) {
   return function (target: any, key: string): void {
     propertyMapper(target, key, (next: string) => {
       if (next.length < min || next.length > max) {
-        throw new Error(`\x1b[91mValidation Error:\x1b[39m on ${target}[${key}] - has length: ${next.length} - should have min: ${min} and max: ${max} length`);
+        throw new Error(`\x1b[91mValidation Error:\x1b[39m on ${target}[${key}] - value has length: ${next.length} - should have min: ${min} and max: ${max} length`);
       } else {
         return next;
       }
