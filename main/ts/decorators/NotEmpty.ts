5b7a23b7f98166e2361547f9138eb9863d2e781f
malkeeszon <rwitczak89@gmail.com>
2020-11-15 17:59:43 +0100

    fix(notempty): fixes decorator to operate on arrays, strings and objects

--- main/ts/decorators/NotEmpty.ts
+++ main/ts/decorators/NotEmpty.ts
@@ -1,14 +1,12 @@
 import propertyMapper from '../helpers/propertyMapper';
 
 export default function NotEmpty() {
-  // eslint-disable-next-line func-names
   return function (target: any, key: string): void {
     propertyMapper(target, key, (next: string) => {
-      if (!next.length) {
-        throw new Error(`\x1b[91mValidation Error:\x1b[39m on ${target}[${key}] - should have length > 0`);
-      } else {
-        return next;
+      if ((!next.length || Object.keys(next).length === 0) && typeof next !== 'number') {
+        throw new Error(`\x1b[91mValidation failed on property: "${key}"\x1b[39m\x1b[93m - value should not be an empty ${typeof next}\x1b[39m`);
       }
+      else return next;
     });
   };
 }
