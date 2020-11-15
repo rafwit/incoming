09fc58d2ee0b1b520089e04053d1c916c0f87f54
malkeeszon <rwitczak89@gmail.com>
2020-11-15 17:54:01 +0100

    fix(islength): fixes decorator to operate on arrays and strings only

--- main/ts/decorators/IsLength.ts
+++ main/ts/decorators/IsLength.ts
@@ -1,14 +1,15 @@
 import propertyMapper from '../helpers/propertyMapper';
 
 export default function IsLength(min: number, max: number) {
-  // eslint-disable-next-line func-names
   return function (target: any, key: string): void {
-    propertyMapper(target, key, (next: string) => {
+    propertyMapper(target, key, (next: string | string[] | number[] | object[]) => {
+      if (!(typeof next === 'string' || Array.isArray(next))) {
+        throw new Error(`\x1b[91mValidation failed on property: "${key}"\x1b[39m\x1b[93m - provided value does not have length\x1b[39m`)
+      }
       if (next.length < min || next.length > max) {
         throw new Error(`\x1b[91mValidation Error:\x1b[39m on ${target}[${key}] - value has length: ${next.length} - should have min: ${min} and max: ${max} length`);
-      } else {
-        return next;
       }
+      else return next;
     });
   };
 }
