103cf3f6cda73fcb248cdfc856628d32424767e7
malkeeszon <rwitczak89@gmail.com>
2020-11-15 17:49:13 +0100

    fix(equals): fixes decorator to throw err if used on type !== string

--- main/ts/decorators/Equals.ts
+++ main/ts/decorators/Equals.ts
@@ -1,11 +1,14 @@
 import propertyMapper from '../helpers/propertyMapper';
 
 export default function Equals(value: string) {
-    return function (target: any, key: string): void {
-        propertyMapper(target, key, (next: string) => {
-            if (next !== value) {
-                throw new Error(`\x1b[91mValidation Error:\x1b[39m on ${target}[${key}] - value schould equal "${value}"`);
-            } else return next;
-        });
-    };
+  return function (target: any, key: string): void {
+    propertyMapper(target, key, (next: string) => {
+      if (typeof next !== 'string') {
+        throw new Error(`\x1b[91mValidation failed on property: "${key}"\x1b[39m\x1b[93m - expected string, received value of type: ${typeof next}\x1b[39m`)
+      }
+      if (next !== value) {
+        throw new Error(`\x1b[91mValidation failed on property: "${key}"\x1b[39m\x1b[93m - value schould equal: ${value}\x1b[39m`);
+      } else return next;
+    });
+  };
 }
